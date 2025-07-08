6d204f67d8f33094f19776759999b47bd8495ab8
somniarch <156504751+somniarch@users.noreply.github.com>
2025-07-08 17:19:36 +0900

    next.config.js

--- my-habit-app/next.config.js
+++ my-habit-app/next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   // Vercel에서는 별도 설정이 없어도 자동으로 최적화됩니다.
   // 필요하다면 기타 옵션만 추가
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "oaidalleapiprodscus.blob.core.windows.net",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
