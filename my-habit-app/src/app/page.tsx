dba1b9e22fc2d4f52b1f47fed3fbdda9f1252694
somniarch <156504751+somniarch@users.noreply.github.com>
2025-07-09 16:44:40 +0900

    Update page.tsx

--- my-habit-app/src/app/page.tsx
+++ my-habit-app/src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image"; 
 import WeeklySummary from "@/components/ui/WeeklySummary";
 import { signIn, signOut, useSession } from "next-auth/react";
